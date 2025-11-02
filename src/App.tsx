65f451e85444516ea2372ec6f07371c6dd0a75cf
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-11-02 16:06:14 +0400

    Added basic structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>CV Maker</h1>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
